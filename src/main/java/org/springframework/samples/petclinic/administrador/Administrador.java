8b11780d10e6d6fe9d6a533707314c6459122398
antoniopelaezmoreno <antpelmor@alum.us.es>
2022-11-10 14:20:54 +0100

    Modificado administrador

--- src/main/java/org/springframework/samples/petclinic/administrador/Administrador.java
+++ src/main/java/org/springframework/samples/petclinic/administrador/Administrador.java
@@ -1,6 +1,7 @@
 package org.springframework.samples.petclinic.administrador;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 import org.springframework.samples.petclinic.user.Usuario;
 
@@ -10,7 +11,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-
+@Table(name = "administradores")
 public class Administrador extends Usuario{
     
 }
