c202c67e723a5e2625e1e18ed8d41f0483f15124
alvvegrod <bjuniorz2001@gmail.com>
2022-11-10 13:31:28 +0100

    Creación entidad de Turnos

---
+++ src/main/java/org/springframework/samples/petclinic/turno/Turno.java
@@ -0,0 +1,41 @@
+package org.springframework.samples.petclinic.turno;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
+
+@Entity
+@Table(name = "turnos")
+public class Turno extends BaseEntity{
+
+    @NotEmpty
+    @Column(name = "dado")
+    private Integer dado; 
+
+    @NotEmpty
+    @Column(name = "puntero")
+    private Integer puntero;
+
+    public Integer getDado() {
+        return dado;
+    }
+
+    public Integer getPuntero() {
+        return puntero;
+    }
+
+    public void setDado(Integer dado) {
+        this.dado = dado;
+    }
+
+    public void setPuntero(Integer puntero) {
+        this.puntero = puntero;
+    } 
+
+
+    
+    
+}
