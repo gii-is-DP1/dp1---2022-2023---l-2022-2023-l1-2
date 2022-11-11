09d8768786c35128a80eeeb2f44064bf055b4e15
alvvegrod <bjuniorz2001@gmail.com>
2022-11-11 12:55:43 +0100

    Corrección

--- src/main/java/org/springframework/samples/petclinic/estadisticasPartida/EstadisticaService.java
+++ src/main/java/org/springframework/samples/petclinic/estadisticasPartida/EstadisticaService.java
@@ -1,21 +1,14 @@
 package org.springframework.samples.petclinic.estadisticasPartida;
 
-<<<<<<< HEAD
-=======
 import org.springframework.beans.factory.annotation.Autowired;
->>>>>>> master
 import org.springframework.stereotype.Service;
 
 @Service
 public class EstadisticaService {
-<<<<<<< HEAD
-    
-=======
     private EstadisticaRepository estadisticaRepository;
 
     @Autowired
 	public EstadisticaService(EstadisticaRepository estadisticaRepository) {
 		this.estadisticaRepository = estadisticaRepository;
 	}
->>>>>>> master
 }
--- src/main/java/org/springframework/samples/petclinic/partida/EstadoPartida.java
+++ src/main/java/org/springframework/samples/petclinic/partida/EstadoPartida.java
@@ -1,9 +1,5 @@
 package org.springframework.samples.petclinic.partida;
 
 public enum EstadoPartida {
-<<<<<<< HEAD
-    LOBBY, ENCURSO, FINALIZADA
-=======
     FINALIZADA, EN_CURSO, EN_COLA
->>>>>>> master
 }
