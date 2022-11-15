6d717062aa893f38cb98194243fe5f581750f0c7
Francisco Fernández Mota <frafermot@alum.us.es>
2022-11-15 19:23:57 +0100

    Join partida edit

--- src/main/java/org/springframework/samples/petclinic/partida/PartidaController.java
+++ src/main/java/org/springframework/samples/petclinic/partida/PartidaController.java
@@ -23,7 +23,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-
 @Controller
 @RequestMapping("/partidas")
 public class PartidaController {
@@ -34,81 +33,70 @@ public class PartidaController {
     private static final String PARTIDA_JOIN = "partidas/joinPartidas";
 
     @Autowired
-    public PartidaController(PartidaService partidaService, UsuarioService usuarioService, JugadorService jugadorService) {
+    public PartidaController(PartidaService partidaService, UsuarioService usuarioService,
+            JugadorService jugadorService) {
         this.partidaService = partidaService;
         this.usuarioService = usuarioService;
         this.jugadorService = jugadorService;
     }
 
-    @GetMapping("/join")
+    @GetMapping("/join/{partidaId}")
     public String initUnirPartida(ModelMap model) {
         String codigo = null;
         model.put("codigo", codigo);
         return PARTIDA_JOIN;
     }
 
-    @PostMapping("/join")
-    public String processUnirPartida(@Valid String codigo, BindingResult result) {
-        String nombreUsuario = null;
-        Optional<Usuario> optUsuario = usuarioService.findUserByNombreUsuario(nombreUsuario);
-        Usuario u = optUsuario.get();
-        if (optUsuario.isPresent() == false) {
-            throw new IllegalArgumentException("No se ha encontrado el nombre de usuario");
-        }
+    @PostMapping("/join/{partidaId}")
+    public String processUnirPartida(Principal principal, @Valid String codigo, BindingResult result,
+            @PathVariable("partidaId") int partidaId) {
         if (result.hasErrors()) {
             return PARTIDA_JOIN;
         } else {
-            if (u.getEsJugador() == false) {
-                u.setEsJugador(true);
-            }
-            Jugador j;
-            j.setId(u.getId());
-            j.setNombreUsuario(u.getNombreUsuario());
-            j.setContraseña(u.getContraseña());
-            j.setFechaNacimiento(u.getFechaNacimiento());
-            j.setEsAdministrador(u.getEsAdministrador());
-            j.setEsJugador(u.getEsJugador());
-            j.setPartidasJugadas(0);
-            j.setPartidasGanadas(0);
-            j.setRecordPuntos(0);
-            j.setTotalPuntos(0);
-            jugadorService.save(j);
-            Optional<Partida> OptP = partidaService.findPartidaByCodigo(codigo);
-            Partida p = OptP.get();
-            if (OptP.isPresent() == false) {
-                throw new IllegalArgumentException("No se ha encontrado la partida");
-            }
-            p.setJugador(p.getJugador().add(j));
-            return "redirect:/partidas/{partidaId}";
+            Partida p = partidaService.findById(partidaId).get();
+            if (!(p.getCodigo().equals(codigo))) {
+                throw new IllegalArgumentException("El código no es correcto");
+                return "redirect:/partidas/join/{partidaId}";
+            } else {
+                Usuario u = usuarioService.findUserByNombreUsuario(principal.getName()).get();
+                Jugador j = jugadorService.findByUsuario(u);
+                j.setUsuario(u);
+                j.setPartidasJugadas(0);
+                j.setPartidasGanadas(0);
+                j.setRecordPuntos(0);
+                j.setTotalPuntos(0);
+                jugadorService.save(j);
+                p.getJugadores().add(j);
+                return "redirect:/partidas/{partidaId}";
+            }   
         }
     }
 
     @GetMapping(value = "/create")
- 	public String initCreationForm(Map<String, Object> model) {
- 		Partida partida = new Partida();
- 		model.put("partida", partida);
- 		return PARTIDA_CREATE;
- 	}
+    public String initCreationForm(Map<String, Object> model) {
+        Partida partida = new Partida();
+        model.put("partida", partida);
+        return PARTIDA_CREATE;
+    }
 
- 	@PostMapping(value = "/create")
-	public String processCreationForm(@Valid Partida partida, BindingResult result) {
- 		if (result.hasErrors()) {
- 			return PARTIDA_CREATE;
- 		}
- 		else {
+    @PostMapping(value = "/create")
+    public String processCreationForm(@Valid Partida partida, BindingResult result) {
+        if (result.hasErrors()) {
+            return PARTIDA_CREATE;
+        } else {
             partida.setFecha(LocalDate.now());
             partida.setEstado(EstadoPartida.EN_COLA);
             partida.setHoraInicio(LocalTime.now());
             this.partidaService.save(partida);
-			
-			return "redirect:/partidas/" + partida.getId();
- 		}
- 	}
+
+            return "redirect:/partidas/" + partida.getId();
+        }
+    }
 
     @GetMapping("/{partidaId}")
- 	public ModelAndView showPartida(@PathVariable("partidaId") int partidaId) {
- 		ModelAndView mav = new ModelAndView("partidas/showPartida");
-			mav.addObject("partida",this.partidaService.findById(partidaId));
-			return mav;
- 	}
+    public ModelAndView showPartida(@PathVariable("partidaId") int partidaId) {
+        ModelAndView mav = new ModelAndView("partidas/showPartida");
+        mav.addObject("partida", this.partidaService.findById(partidaId));
+        return mav;
+    }
 }
