31d6e8e94e6a96fba1a9f65c85a298de27e18e0e
antoniopelaezmoreno <antpelmor@alum.us.es>
2022-11-15 16:50:52 +0100

    Añadidos creador y jugadores a la partida

--- src/main/java/org/springframework/samples/petclinic/partida/PartidaController.java
+++ src/main/java/org/springframework/samples/petclinic/partida/PartidaController.java
@@ -3,12 +3,19 @@ package org.springframework.samples.petclinic.partida;
 import java.security.Principal;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.Set;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.jugador.Jugador;
+import org.springframework.samples.petclinic.jugador.JugadorService;
+import org.springframework.samples.petclinic.usuario.Usuario;
+import org.springframework.samples.petclinic.usuario.UsuarioService;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,11 +29,15 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/partidas")
 public class PartidaController {
     private final PartidaService partidaService;
+	private final JugadorService jugadorService;
+	private final UsuarioService usuarioService;
     private static final String PARTIDA_CREATE = "partidas/createPartidas";
 
     @Autowired
-	public PartidaController(PartidaService partidaService) {
+	public PartidaController(PartidaService partidaService, JugadorService jugadorService, UsuarioService usuarioService) {
 		this.partidaService = partidaService;
+		this.jugadorService = jugadorService;
+		this.usuarioService = usuarioService;
 	}
 
 
@@ -38,14 +49,21 @@ public class PartidaController {
  	}
 
  	@PostMapping(value = "/create")
-	public String processCreationForm(@Valid Partida partida, BindingResult result) {
+	public String processCreationForm(Principal principal, @Valid Partida partida, BindingResult result) {
  		if (result.hasErrors()) {
  			return PARTIDA_CREATE;
  		}
  		else {
+			Optional<Usuario> user = usuarioService.findUsuario(principal.getName());
+			Jugador jug = jugadorService.findByUsuario(user.get());
             partida.setFecha(LocalDate.now());
             partida.setEstado(EstadoPartida.EN_COLA);
             partida.setHoraInicio(LocalTime.now());
+			partida.setCreador(jug);
+			Set<Jugador> set = new HashSet<>();
+			set.add(jug);
+			partida.setJugadores(set);
+
             this.partidaService.save(partida);
 			
 			return "redirect:/partidas/" + partida.getId();
