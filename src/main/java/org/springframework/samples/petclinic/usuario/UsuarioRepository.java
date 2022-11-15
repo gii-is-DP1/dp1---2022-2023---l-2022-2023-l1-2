ef5f78fd886fb8f439776f0fb5a96a26afe3c283
carlosdelrioperez <carlosmix52@gmail.com>
2022-11-15 10:34:58 +0100

    commit

--- src/main/java/org/springframework/samples/petclinic/usuario/UsuarioRepository.java
+++ src/main/java/org/springframework/samples/petclinic/usuario/UsuarioRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, Integer>{
-    List<Usuario> findAll();
+    List<Usuario> findAll();   
 }
