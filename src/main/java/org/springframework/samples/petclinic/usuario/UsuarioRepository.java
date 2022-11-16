63ce7034049ef578f9b78558b4bbc555fea26a3c
Gabriel Vidal <gabvidtev@alum.us.es>
2022-11-16 16:19:56 +0100

    Corregido error

--- src/main/java/org/springframework/samples/petclinic/usuario/UsuarioRepository.java
+++ src/main/java/org/springframework/samples/petclinic/usuario/UsuarioRepository.java
@@ -1,14 +1,17 @@
 package org.springframework.samples.petclinic.usuario;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 
 
-public interface UsuarioRepository extends CrudRepository<Usuario, String>{
+
 
 @Repository
-public interface UsuarioRepository extends CrudRepository<Usuario, Integer>{
+public interface UsuarioRepository extends CrudRepository<Usuario, String>{
     List<Usuario> findAll();   
 }
